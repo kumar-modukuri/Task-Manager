dff10bba1fe7d6ee5fa2e88d23d7e32cfa57d75b
kumar-modukuri <rajkumar87901311@gmail.com>
2024-11-14 13:38:25 +0530

    Initial

---
+++ src/components/middle/Form.js
@@ -0,0 +1,94 @@
+import { useState } from "react";
+import "./FormStyles.css";
+
+const Form = () => {
+	const [title, setTitle] = useState("");
+	const [description, setDescription] = useState("");
+	const [loading, setLoading] = useState(false);
+	const [priority, setPriority] = useState("default");
+
+	const handleAdd = async (e) => {
+		e.preventDefault();
+		setLoading(true);
+
+		try {
+			const id = Date.now();
+			const newTask = { id, title, description, priority };
+
+			const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+			tasks.push(newTask);
+
+			localStorage.setItem("tasks", JSON.stringify(tasks));
+
+			console.log("Task Added : ", newTask);
+		} catch (error) {
+			console.log("Error While Adding Task :", error);
+		} finally {
+			setLoading(false);
+			setTitle("");
+			setDescription("");
+			setPriority("default");
+		}
+	};
+
+	return (
+		<form onSubmit={handleAdd}>
+			<label>Title : </label>
+			<input
+				type="text"
+				placeholder="Type here"
+				value={title}
+				onChange={(e) => setTitle(e.target.value)}
+				required
+			/>
+			<label>Description : </label>
+			<textarea
+				rows="8"
+				placeholder="Type here"
+				value={description}
+				onChange={(e) => setDescription(e.target.value)}
+				required></textarea>
+			<div className="priority">
+				<div>
+					<input
+						type="radio"
+						id="high"
+						name="priority"
+						value="high"
+						checked={priority === "high"}
+						onChange={(e) => setPriority(e.target.value)}
+					/>
+					<label htmlFor="high">High</label>
+				</div>
+				<div>
+					<input
+						type="radio"
+						id="medium"
+						name="priority"
+						value="medium"
+						checked={priority === "medium"}
+						onChange={(e) => setPriority(e.target.value)}
+					/>
+					<label htmlFor="medium">Medium</label>
+				</div>
+				<div>
+					<input
+						type="radio"
+						id="low"
+						name="priority"
+						value="low"
+						checked={priority === "low"}
+						onChange={(e) => setPriority(e.target.value)}
+					/>
+					<label htmlFor="low">Low</label>
+				</div>
+			</div>
+			<button type="submit" className="btn" disabled={loading}>
+				{loading ? "ADDING..." : "ADD"}
+			</button>
+		</form>
+	);
+};
+
+export default Form;
--- src/components/middle/Middle.js
+++ src/components/middle/Middle.js
@@ -1,9 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { GrPowerReset } from "react-icons/gr";
 import "../../index.css";
+import Form from "./Form.js";
 import "./MiddleStyles.css";
 
 const Middle = () => {
 	const [searched, setSearched] = useState("");
+	const [filters, setFilters] = useState([]);
+
+	const handleFilterChange = (filter) => {
+		setFilters((prevFilters) => {
+			if (prevFilters.includes(filter)) {
+				return prevFilters.filter((f) => f !== filter);
+			} else {
+				return [...prevFilters, filter];
+			}
+		});
+	};
+
+	const handleReset = () => {
+		setFilters([]);
+	};
+
+	useEffect(() => {
+		console.log(filters);
+	}, [filters]);
 
 	return (
 		<div className="middle-container">
@@ -14,9 +35,69 @@ const Middle = () => {
 					value={searched}
 					onChange={(e) => setSearched(e.target.value)}
 				/>
-				<button>SEARCH</button>
+				<button className="btn">SEARCH</button>
 			</div>
-			<div className="content-div">Content</div>
+			<div className="content-div">
+				<div className="tasks-div">
+					<div className="filters-div">
+						<div>
+							<input
+								type="checkbox"
+								onChange={() => handleFilterChange("High")}
+								checked={filters.includes("High")}
+							/>
+							<label>High</label>
+						</div>
+						<div>
+							<input
+								type="checkbox"
+								onChange={() => handleFilterChange("Medium")}
+								checked={filters.includes("Medium")}
+							/>
+							<label>Medium</label>
+						</div>
+						<div>
+							<input
+								type="checkbox"
+								onChange={() => handleFilterChange("Low")}
+								checked={filters.includes("Low")}
+							/>
+							<label>Low</label>
+						</div>
+						<div>
+							<input
+								type="checkbox"
+								onChange={() => handleFilterChange("Default")}
+								checked={filters.includes("Default")}
+							/>
+							<label>Default</label>
+						</div>
+						<div>
+							<input
+								type="checkbox"
+								onChange={() => handleFilterChange("Completed")}
+								checked={filters.includes("Completed")}
+							/>
+							<label>Completed</label>
+						</div>
+						<div>
+							<input
+								type="checkbox"
+								onChange={() => handleFilterChange("Not Completed")}
+								checked={filters.includes("Not Completed")}
+							/>
+							<label>Not Completed</label>
+						</div>
+						<div style={{ cursor: "pointer" }} onClick={handleReset}>
+							<GrPowerReset color="#4650fa" size={20} />
+						</div>
+					</div>
+					<div className="tasks-container"></div>
+				</div>
+				<div className="form-div">
+					<Form />
+				</div>
+			</div>
 		</div>
 	);
 };
